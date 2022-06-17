e4d1b55b34e2fe6275693a19ba728f940b386efd
septem.one71 <septem.one71@gmail.com>
2022-06-18 02:39:38 +0900

    lint忘れ

--- src/components/padding/Padding.tsx
+++ src/components/padding/Padding.tsx
@@ -1,14 +1,5 @@
 import React, { useState } from 'react'
-import {
-    Slider,
-    SliderTrack,
-    SliderFilledTrack,
-    SliderThumb,
-    SliderMark,
-    Tooltip,
-    Flex,
-    Text
-  } from '@chakra-ui/react'
+import { Slider, SliderTrack, SliderFilledTrack, SliderThumb, SliderMark, Tooltip, Flex, Text } from '@chakra-ui/react'
 import { useAppDispatch, useAppSelector } from '../../hooks'
 import { setPadding } from '../buttonView/buttonViewSlice'
 
@@ -16,42 +7,46 @@ export const Padding = () => {
     const dispatch = useAppDispatch()
     const padding = useAppSelector((state) => state.buttonView.padding)
     const [showTooltip, setShowTooltip] = useState(false)
-    const onChangeValue = (v:number) => {
-        dispatch(setPadding(v.toString()+'px'))
+    const onChangeValue = (v: number) => {
+        dispatch(setPadding(v.toString() + 'px'))
     }
     return (
-        <Flex flexDirection={'column'} alignItems={'center'} margin={'1rem'} padding={'1rem'} width={'100%'} border={'1px'} borderRadius={'1rem'} borderColor={'gray.200'}>
-            <Text color={'black'} marginBottom={'1rem'}>Padding</Text>
+        <Flex
+            flexDirection={'column'}
+            alignItems={'center'}
+            margin={'1rem'}
+            padding={'1rem'}
+            width={'100%'}
+            border={'1px'}
+            borderRadius={'1rem'}
+            borderColor={'gray.200'}
+        >
+            <Text color={'black'} marginBottom={'1rem'}>
+                Padding
+            </Text>
             <Slider
-            id='slider'
-            defaultValue={25}
-            min={0}
-            max={200}
-            colorScheme='teal'
-            onChange={(v) => onChangeValue(v)}
-            onMouseEnter={() => setShowTooltip(true)}
-            onMouseLeave={() => setShowTooltip(false)}
+                id="slider"
+                defaultValue={25}
+                min={0}
+                max={200}
+                colorScheme="teal"
+                onChange={(v) => onChangeValue(v)}
+                onMouseEnter={() => setShowTooltip(true)}
+                onMouseLeave={() => setShowTooltip(false)}
             >
-                <SliderMark color={'black'} value={50} mt='1' ml='-2.5' fontSize='sm'>
+                <SliderMark color={'black'} value={50} mt="1" ml="-2.5" fontSize="sm">
                     50px
                 </SliderMark>
-                <SliderMark color={'black'} value={100} mt='1' ml='-2.5' fontSize='sm'>
+                <SliderMark color={'black'} value={100} mt="1" ml="-2.5" fontSize="sm">
                     100px
                 </SliderMark>
-                <SliderMark color={'black'} value={150} mt='1' ml='-2.5' fontSize='sm'>
+                <SliderMark color={'black'} value={150} mt="1" ml="-2.5" fontSize="sm">
                     150px
                 </SliderMark>
                 <SliderTrack bg={'teal.50'}>
                     <SliderFilledTrack />
                 </SliderTrack>
-                <Tooltip
-                    hasArrow
-                    bg='teal.500'
-                    color='white'
-                    placement='top'
-                    isOpen={showTooltip}
-                    label={`${padding}`}
-                >
+                <Tooltip hasArrow bg="teal.500" color="white" placement="top" isOpen={showTooltip} label={`${padding}`}>
                     <SliderThumb />
                 </Tooltip>
             </Slider>
