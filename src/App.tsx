026b494f3bb54c43d03a6e7835f5acd26f52ecaf
Actlam <stream.actlam@gmail.com>
2022-06-21 02:19:01 +0900

    add: headerの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,45 @@
-import { ChakraProvider, Flex } from '@chakra-ui/react'
+import { Center, ChakraProvider, Flex } from '@chakra-ui/react'
 import React from 'react'
 import { Provider } from 'react-redux'
 import { BackgroundColor } from './components/backgroundColor/BackgroundColor'
 import { ButtonView } from './components/buttonView/ButtonView'
 import { ChangeColor } from './components/changeColor/ChangeColor'
-import { store } from './store'
+import { CodeArea } from './components/codeArea/CodeArea'
+import Header from './components/header/Header'
 import { Padding } from './components/padding/Padding'
 import { Templates } from './components/templates/Templates'
-import { CodeArea } from './components/codeArea/CodeArea'
+import { store } from './store'
 
 const App = () => {
     return (
         <Provider store={store}>
             <ChakraProvider>
-                <Flex flexDirection={'row'} justifyContent={'center'} width={'100%'} height={'60rem'}>
-                    <Flex
-                        flexDirection={'column'}
-                        alignItems={'center'}
-                        bg={'white'}
-                        minWidth={'20rem'}
-                        maxWidth={'40rem'}
-                        flex={1}
-                    >
-                        <ChangeColor />
-                        <BackgroundColor />
-                        <Padding />
-                    </Flex>
-                    <Flex
-                        flexDirection={'column'}
-                        alignItems={'center'}
-                        justifyContent={'center'}
-                        bg={'black'}
-                        minWidth={'30rem'}
-                        maxWidth={'120rem'}
-                        flex={3}
-                    >
-                        <ButtonView />
-                        <Templates />
-                    </Flex>
-                    <Flex
-                        flexDirection={'column'}
-                        alignItems={'center'}
-                        justifyContent={'center'}
-                        bg={'gray'}
-                        minWidth={'30rem'}
-                        maxWidth={'120rem'}
-                        flex={3}
-                    >
-                        <CodeArea />
+                <Flex flexDirection="column">
+                    <Header />
+                    <Flex flexDirection={'row'} justifyContent={'center'} width={'100%'} height={'60rem'}>
+                        <Flex flexDirection={'column'} alignItems={'center'} bg={'white'} width="50%" flex={1}>
+                            <ChangeColor />
+                            <BackgroundColor />
+                            <Padding />
+                        </Flex>
+                        <Flex
+                            flexDirection={'column'}
+                            alignItems={'center'}
+                            justifyContent={'center'}
+                            bg={'gray'}
+                            width="50%"
+                            flex={3}
+                        >
+                            <Center>
+                                <ButtonView />
+                            </Center>
+                            <Center>
+                                <CodeArea />
+                            </Center>
+                            <Center>
+                                <Templates />
+                            </Center>
+                        </Flex>
                     </Flex>
                 </Flex>
             </ChakraProvider>
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import { Center } from '@chakra-ui/react'
+import React from 'react'
+
+const Header = () => {
+    return (
+        <Center as="h1" bg="white.300" fontWeight="bold" fontSize="large" w="100%" p={4}>
+            Varia UI
+        </Center>
+    )
+}
+
+export default Header
