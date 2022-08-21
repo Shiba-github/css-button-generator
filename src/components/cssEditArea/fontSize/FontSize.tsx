b5fb099c9b5a773c85b1e4a4a6c577ad8e7ce2d0
Actlam <stream.actlam@gmail.com>
2022-08-21 23:18:28 +0900

    add: カスタムエリアにfontsizeのオンオフボタンを追加

--- src/components/cssEditArea/fontSize/FontSize.tsx
+++ src/components/cssEditArea/fontSize/FontSize.tsx
@@ -7,50 +7,64 @@ export const FontSize = () => {
     const dispatch = useAppDispatch()
     const fontSize = useAppSelector((state) => state.buttonView.fontSize)
     const [showTooltip, setShowTooltip] = useState(false)
+    const displayFontSize = useAppSelector((state) => state.cssCustomArea.displayFontSize)
     const onChangeValue = (v: number) => {
         dispatch(setFontSize(v.toString() + 'px'))
     }
     return (
-        <Flex
-            flexDirection={'column'}
-            alignItems={'center'}
-            margin={'1rem'}
-            padding={'1rem'}
-            width={'50rem'}
-            border={'1px'}
-            borderRadius={'1rem'}
-            borderColor={'gray.200'}
-        >
-            <Text color={'black'} marginBottom={'1rem'} fontSize={'2rem'}>
-                FontSize
-            </Text>
-            <Slider
-                id="slider"
-                defaultValue={25}
-                value={parseInt(fontSize.replace('px', ''))}
-                min={0}
-                max={200}
-                colorScheme="teal"
-                onChange={(v) => onChangeValue(v)}
-                onMouseEnter={() => setShowTooltip(true)}
-                onMouseLeave={() => setShowTooltip(false)}
-            >
-                <SliderMark color={'black'} value={50} mt="1" ml="-2.5" fontSize="sm">
-                    50px
-                </SliderMark>
-                <SliderMark color={'black'} value={100} mt="1" ml="-2.5" fontSize="sm">
-                    100px
-                </SliderMark>
-                <SliderMark color={'black'} value={150} mt="1" ml="-2.5" fontSize="sm">
-                    150px
-                </SliderMark>
-                <SliderTrack bg={'teal.50'}>
-                    <SliderFilledTrack />
-                </SliderTrack>
-                <Tooltip hasArrow bg="teal.500" color="white" placement="top" isOpen={showTooltip} label={`${fontSize}`}>
-                    <SliderThumb />
-                </Tooltip>
-            </Slider>
-        </Flex>
+        <>
+            {displayFontSize ? (
+                <Flex
+                    flexDirection={'column'}
+                    alignItems={'center'}
+                    margin={'1rem'}
+                    padding={'1rem'}
+                    width={'50rem'}
+                    border={'1px'}
+                    borderRadius={'1rem'}
+                    borderColor={'gray.200'}
+                >
+                    <Text color={'black'} marginBottom={'1rem'} fontSize={'2rem'}>
+                        FontSize
+                    </Text>
+                    <Slider
+                        id="slider"
+                        defaultValue={25}
+                        value={parseInt(fontSize.replace('px', ''))}
+                        min={0}
+                        max={200}
+                        colorScheme="teal"
+                        onChange={(v) => onChangeValue(v)}
+                        onMouseEnter={() => setShowTooltip(true)}
+                        onMouseLeave={() => setShowTooltip(false)}
+                    >
+                        <SliderMark color={'black'} value={50} mt="1" ml="-2.5" fontSize="sm">
+                            50px
+                        </SliderMark>
+                        <SliderMark color={'black'} value={100} mt="1" ml="-2.5" fontSize="sm">
+                            100px
+                        </SliderMark>
+                        <SliderMark color={'black'} value={150} mt="1" ml="-2.5" fontSize="sm">
+                            150px
+                        </SliderMark>
+                        <SliderTrack bg={'teal.50'}>
+                            <SliderFilledTrack />
+                        </SliderTrack>
+                        <Tooltip
+                            hasArrow
+                            bg="teal.500"
+                            color="white"
+                            placement="top"
+                            isOpen={showTooltip}
+                            label={`${fontSize}`}
+                        >
+                            <SliderThumb />
+                        </Tooltip>
+                    </Slider>
+                </Flex>
+            ) : (
+                ''
+            )}
+        </>
     )
 }
