41333c212cb4cbdc6105e04ed3e79c884785c836
shiba <septem.one71@gmail.com>
2023-01-01 17:04:18 +0900

    templateを古い書き方から新しい書き方に変更しました

--- src/components/templates/borderGradientButton/BorderGradientButton.tsx
+++ src/components/templates/borderGradientButton/BorderGradientButton.tsx
@@ -1,58 +1,26 @@
 import React from 'react'
-import { css, CSSObject } from '@emotion/react'
-import { Flex } from '@chakra-ui/react'
-import { useAppDispatch } from '../../../hooks'
-import {
-    setWidth,
-    setHeight,
-    setBackgroundColor,
-    setBorder,
-    setColor,
-    setFontSize,
-} from '../../buttonView/buttonViewSlice'
+import { ButtonTemplate } from '../ButtonTemplate'
 
 // TODO：コメントアウトしてるプロパティはストア側で未対応なので要実装
+const buttonStyle = {
+    width: '140px',
+    height: '60px',
+    backgroundColor: '#000',
+    borderWidth: '4px',
+    borderStyle: 'solid',
+    // borderImage: 'linear-gradient(140deg, rgba(2,0,36,1) 0%, rgba(9,9,120,1) 35%, rgba(0,211,255,1) 100%)',
+    // borderImageSlice: '1',
+    color: '#fff',
+    fontSize: '14px',
+    // letterSpacing: '2px',
+    '&:hover': {
+        // backgroundImage: 'linear-gradient(140deg, rgba(2,0,36,1) 0%, rgba(9,9,120,1) 35%, rgba(0,211,255,1) 100%)',
+    },
+    '&:active': {
+        transform: 'scale(0.95)',
+    },
+}
+
 export const BorderGradientButton = () => {
-    const dispatch = useAppDispatch()
-    const copyCssProps = () => {
-        dispatch(setWidth(buttonStyle.width))
-        dispatch(setHeight(buttonStyle.height))
-        dispatch(setBackgroundColor(buttonStyle.backgroundColor))
-        dispatch(setBorder(buttonStyle.border))
-        // dispatch(setBorderImage(buttonStyle.borderImage))
-        // dispatch(setBorderImageSlice(buttonStyle.borderImageSlice))
-        dispatch(setColor(buttonStyle.color))
-        dispatch(setFontSize(buttonStyle.fontSize))
-        // dispatch(setLetterSpacing(buttonStyle.letterSpacing))
-    }
-    const buttonStyle = {
-        width: '140px',
-        height: '60px',
-        backgroundColor: '#000',
-        border: '4px solid;',
-        borderImage: 'linear-gradient(140deg, rgba(2,0,36,1) 0%, rgba(9,9,120,1) 35%, rgba(0,211,255,1) 100%)',
-        borderImageSlice: '1',
-        color: '#fff',
-        fontSize: '14px',
-        letterSpacing: '2px',
-        '&:hover': {
-            backgroundImage: 'linear-gradient(140deg, rgba(2,0,36,1) 0%, rgba(9,9,120,1) 35%, rgba(0,211,255,1) 100%)',
-        },
-        '&:active': {
-            transform: 'scale(0.95)',
-        },
-    }
-
-    const buttonStyles = {
-        ...buttonStyle,
-    } as CSSObject
-    const cssProps = css(buttonStyles)
-
-    return (
-        <Flex flexDirection={'row'} alignItems={'center'} justifyContent={'center'}>
-            <button css={cssProps} onClick={() => copyCssProps()}>
-                Button
-            </button>
-        </Flex>
-    )
+    return <ButtonTemplate {...buttonStyle} />
 }
--- src/components/templates/simpleRoundIconButton/SimpleRoundIcon.tsx
+++ src/components/templates/simpleRoundIconButton/SimpleRoundIcon.tsx
@@ -1,92 +1,73 @@
-import React, { CSSProperties, useState } from 'react'
-import { Box, Button, Flex } from '@chakra-ui/react'
-import { useAppDispatch } from '../../../hooks'
-import {
-    setBackgroundColor,
-    setBorderColor,
-    setBorderRadius,
-    setBorderStyle,
-    setBorderWidth,
-    setColor,
-    setHeight,
-    setPadding,
-    setWidth,
-} from '../../buttonView/buttonViewSlice'
-import { motion } from 'framer-motion'
-import { SimpleRoundIconButtonVariants } from './SimpleRoundIconButtonAnimation'
+import React from 'react'
+import { ButtonTemplate } from '../ButtonTemplate'
 
-import home from './home.png'
-
-export const SimpleRoundIconButton = () => {
-    // TODO：コメントアウトのものはストアに未実装のプロパティ
-    // animationは未実装なので、一旦framer motionにて仮実装しておきます
-    const dispatch = useAppDispatch()
-    const [isHover, setIsHover] = useState(false)
-    const onClick = () => {
-        dispatch(setWidth(buttonStyle.width))
-        dispatch(setHeight(buttonStyle.height))
-        dispatch(setBackgroundColor(buttonStyle.backgroundColor))
-        dispatch(setBorderRadius(buttonStyle.borderRadius))
-        dispatch(setBorderStyle(buttonStyle.borderStyle))
-        dispatch(setBorderWidth(buttonStyle.borderWidth))
-        dispatch(setBorderColor(buttonStyle.borderColor))
-        // dispatch(setBoxShadow(buttonStyle.boxShadow))
-        dispatch(setColor(buttonStyle.color))
-        // dispatch(setCursor(buttonStyle.cursor))
-        dispatch(setPadding(buttonStyle.padding))
-        // dispatch(setBackgroundImage(buttonStyle.backgroundImage))
-        // dispatch(setBackgroundSize(buttonStyle.backgroundSize))
-        // dispatch(setBackgroundRepeat(buttonStyle.backgroundRepeat))
-        // dispatch(setBackgroundPosition(buttonStyle.backgroundPosition))
-    }
-    const buttonStyle = {
+const buttonStyle = {
+    width: '70px',
+    height: '70px',
+    backgroundColor: 'transparent',
+    // backgroundImage: `url(${home})`,
+    borderRadius: '50%',
+    borderStyle: 'solid',
+    borderWidth: '3px',
+    borderColor: '#1FE88A',
+    // boxShadow: 'rgba(0, 0, 0, .2) 15px 28px 25px -18px',
+    color: '#41403e',
+    // cursor: 'pointer',
+    padding: '.75rem',
+    // backgroundSize: '70%',
+    // backgroundRepeat: 'no-repeat',
+    // backgroundPosition: 'center',
+    '&::before': {
+        // content: '',
         width: '70px',
         height: '70px',
-        backgroundColor: 'transparent',
-        backgroundImage: `url(${home})`,
         borderRadius: '50%',
         borderStyle: 'solid',
         borderWidth: '3px',
         borderColor: '#1FE88A',
-        boxShadow: 'rgba(0, 0, 0, .2) 15px 28px 25px -18px',
-        color: '#41403e',
-        cursor: 'pointer',
-        padding: '.75rem',
-        backgroundSize: '70%',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-    }
+        backgroundColor: 'rgb(70 68 94)',
+    },
+    '&:hover::before': {
+        borderWidth: '35px',
+    },
+    '&:hover': {
+        // boxShadow: 'rgba(0, 0, 0, .3) 2px 8px 8px -5px',
+        // transform: 'translate3d(0, 2px, 0)',
+    },
+    '&:active': {
+        // boxShadow: 'rgba(0, 0, 0, .3) 0px 0px 0px 0px',
+        // transform: 'translate3d(0, 2px, 0)',
+    },
+}
 
-    const pseudoStyle: CSSProperties = {
-        position: 'absolute',
-    }
-
-    return (
-        <Flex flexDirection={'row'} alignItems={'center'} justifyContent={'center'}>
-            {/* cssにしたときはbeforeで代用予定 */}
-            <Box
-                as={motion.div}
-                variants={SimpleRoundIconButtonVariants}
-                initial={SimpleRoundIconButtonVariants.pseudoInit}
-                animate={
-                    isHover ? SimpleRoundIconButtonVariants.pseudoHover : SimpleRoundIconButtonVariants.pseudoHoverOut
-                }
-                whileHover={
-                    isHover ? SimpleRoundIconButtonVariants.pseudoHover : SimpleRoundIconButtonVariants.pseudoHoverOut
-                }
-                style={pseudoStyle}
-            />
-            <Button
-                as={motion.button}
-                variants={SimpleRoundIconButtonVariants}
-                initial={SimpleRoundIconButtonVariants.off}
-                whileHover={SimpleRoundIconButtonVariants.hover}
-                whileTap={SimpleRoundIconButtonVariants.tap}
-                style={buttonStyle}
-                onClick={() => onClick()}
-                onHoverStart={() => setIsHover(true)}
-                onHoverEnd={() => setIsHover(false)}
-            ></Button>
-        </Flex>
-    )
+export const SimpleRoundIconButton = () => {
+    return <ButtonTemplate {...buttonStyle} />
+    // return (
+    //     <Flex flexDirection={'row'} alignItems={'center'} justifyContent={'center'}>
+    //         {/* cssにしたときはbeforeで代用予定 */}
+    //         <Box
+    //             as={motion.div}
+    //             variants={SimpleRoundIconButtonVariants}
+    //             initial={SimpleRoundIconButtonVariants.pseudoInit}
+    //             animate={
+    //                 isHover ? SimpleRoundIconButtonVariants.pseudoHover : SimpleRoundIconButtonVariants.pseudoHoverOut
+    //             }
+    //             whileHover={
+    //                 isHover ? SimpleRoundIconButtonVariants.pseudoHover : SimpleRoundIconButtonVariants.pseudoHoverOut
+    //             }
+    //             style={pseudoStyle}
+    //         />
+    //         <Button
+    //             as={motion.button}
+    //             variants={SimpleRoundIconButtonVariants}
+    //             initial={SimpleRoundIconButtonVariants.off}
+    //             whileHover={SimpleRoundIconButtonVariants.hover}
+    //             whileTap={SimpleRoundIconButtonVariants.tap}
+    //             style={buttonStyle}
+    //             onClick={() => onClick()}
+    //             onHoverStart={() => setIsHover(true)}
+    //             onHoverEnd={() => setIsHover(false)}
+    //         ></Button>
+    //     </Flex>
+    // )
 }
